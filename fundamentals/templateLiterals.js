cc295929d3491c891baa397386594d006d6d513d
Ladzi <ladziladzani@gmail.com>
2023-06-16 17:14:26 +0200

    some revision on String Templates

--- fundamentals/templateLiterals.js
+++ fundamentals/templateLiterals.js
@@ -10,3 +10,10 @@ let quotes = `you can next 'single' and "double" quotes inside javaScript-templa
 
 console.log(quotes);
 console.log(template);
+
+let price = 10;
+let VAT = 0.25;
+
+let total = `Total: ${(price * (1 + VAT)).toFixed(2)}`;
+
+console.log(total) // results: 'Total: 12.50'
