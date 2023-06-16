96038c9af71f5d2e7d5136a625fbcc217e63175f
Ladzi <ladziladzani@gmail.com>
2023-06-16 07:50:14 +0200

    revised variable concepts

--- fundamentals/main.js
+++ fundamentals/main.js
@@ -2,7 +2,18 @@ console.log("hello world"); // part of the web-browser...
 console.log('hello world') // no need for semi-colon, and use of single-quotes
 
 
-// initialising variables:
+/* 
+    For character sets javascript uses unicode character sets 
+*/
+
+/* 
+    initialising variables:
+    Using var ... does not have block scope
+    
+    Using let .... block-scope
+
+    Using const ... block scope, once assigned a value, it cannot be changed!
+*/
 var a;
 let b;
 
@@ -11,6 +22,7 @@ a = 5;
 b = 6;
 let z = x + y;
 //console.log(z);
+
 
 // not so clean-code...
 function pairElement(str) {
