151e61b504c4cc6c85b400c7b0ef51493e8080b9
Ladzi <ladziladzani@gmail.com>
2023-06-16 17:16:33 +0200

    some revision on String Templates

--- fundamentals/templateLiterals.js
+++ fundamentals/templateLiterals.js
@@ -17,3 +17,14 @@ let VAT = 0.25;
 let total = `Total: ${(price * (1 + VAT)).toFixed(2)}`;
 
 console.log(total) // results: 'Total: 12.50'
+
+
+let header = "Templates Literals";
+let tags = ["template literals", "javascript", "es6"];
+
+let html = `<h2>${header}</h2><ul>`;
+for (const x of tags) {
+  html += `<li>${x}</li>`;
+}
+
+html += `</ul>`;
