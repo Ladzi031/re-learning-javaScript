414c2c326f710450125839c2d410f805a690d045
Ladzi <ladziladzani@gmail.com>
2023-06-16 09:10:50 +0200

    revision

--- fundamentals/main.js
+++ fundamentals/main.js
@@ -24,34 +24,3 @@ let z = x + y;
 //console.log(z);
 
 
-// not so clean-code...
-function pairElement(str) {
-  let arr = str.split("");
-  let newArr = arr.map(x => {
-    let shortString = x;
-    switch(x){
-      case "G":
-      shortString += "C";
-     return shortString.split("");
-      break;
-
-      case "C":
-      shortString += "G";
-      return shortString.split("");
-      break;
-
-      case "A":
-      shortString += "T";
-      return shortString.split("");
-      break;
-
-      case "T":
-      shortString += "A";
-      return shortString.split("");
-      break;
-    }
-  })
-  //console.log(newArr); testing here... 
-  return newArr;
-}
-pairElement("CTCTA"); // DNA pairing function, that pairs each DNA nucleotide with its respective nucleotide
