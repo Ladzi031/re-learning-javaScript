cc25815445cb8cc7c58d7f68e05417923f15e637
Ladzi <ladziladzani@gmail.com>
2023-06-16 08:17:04 +0200

    revised javascript datatypes with w3schools

--- fundamentals/datatypes.js
+++ fundamentals/datatypes.js
@@ -24,18 +24,37 @@ The  OBJECT data type can contain:
 
 let x = "Volvo" + 16 + 4; // results: Volvo164
 
-
-let a;       // Now x is undefined
-a = 5;       // Now x is a Number
-a = "John";  // Now x is a String 
+let a; // Now x is undefined
+a = 5; // Now x is a Number
+a = "John"; // Now x is a String
 
 // exponents
-let y = 123e5;    // 12300000
-let z = 123e-5;   // 0.00123 
+let y = 123e5; // 12300000
+let z = 123e-5; // 0.00123
 
 // With decimals:
-let x1 = 34.00;
+let x1 = 34.0;
 
 // Without decimals:
 let x2 = 34;
 let x3 = x1 + x2;
+
+// basic boolean concepts
+let t = 5;
+let r = 5;
+let c = 6;
+ (t == r)  // Returns true
+(t == c) // Returns false
+// mostly used for testing conditions
+
+//a javaScript array...
+const cars = ["Saab", "Volvo", "BMW"];
+
+//a javascript object...
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
+let g = undefined; // this is valid in javascript
