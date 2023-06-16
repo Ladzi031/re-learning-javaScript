0647c21daba5b341b5197237688cb1ed2d6fac9c
Ladzi <ladziladzani@gmail.com>
2023-06-16 17:12:52 +0200

    some revision on String Templates

---
+++ fundamentals/templateLiterals.js
@@ -0,0 +1,12 @@
+
+const idNumbers = 242424242;
+
+let template = `this is a template literal...`;
+
+let details = `my id-number is ${idNumbers}`;
+
+let quotes = `you can next 'single' and "double" quotes inside javaScript-templates"`;
+
+
+console.log(quotes);
+console.log(template);
