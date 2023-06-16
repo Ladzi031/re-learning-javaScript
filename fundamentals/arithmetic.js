0c221213b58f3cff0bbafc50981dd89e2ae7eeb6
Ladzi <ladziladzani@gmail.com>
2023-06-16 07:58:34 +0200

    revised arithmetic operators

---
+++ fundamentals/arithmetic.js
@@ -0,0 +1,31 @@
+/*
++ :	Addition
+- :	Subtraction
+* :	Multiplication
+** : Exponentiation (ES2016)
+/ :	Division
+% :	Modulus (Remainder)
+++ :	Increment
+-- :	Decrement
+*/
+
+let x = 100 + 50;
+//console.log(x);
+const a = 3;
+const b = 5;
+let y = a + b;
+let q = (100 + 50) * a; // js expression
+
+x = 100 / 50 * 3; // js expression
+ 
+
+/*
+    
+= : 	x = y      	x = y
++= :    x += y  	x = x + y
+-= :    x -= y  	x = x - y
+*= :    x *= y  	x = x * y
+/= :    x /= y  	x = x / y
+%= :    x %= y  	x = x % y
+**= :   x **= y 	x = x ** y
+ */
