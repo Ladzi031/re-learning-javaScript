660e1b6336f8f8791bd900717bd712d28f4bacc9
Ladzi <ladziladzani@gmail.com>
2023-06-16 09:02:13 +0200

    revise javaScript fundamentals

--- fundamentals/arrays.js
+++ fundamentals/arrays.js
@@ -10,6 +10,15 @@ The unique numbers should be sorted by their original order, but the final array
 Check the assertion tests for examples.
 
 */
+            // problem domain
+  /*
+  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+  uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+  uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
+  uniteUnique([1, 3, 2], [5, 4], [5, 6]) should return [1, 3, 2, 5, 4, 6].
+  uniteUnique([1, 3, 2, 3], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+  */
+ 
 function uniteUnique(arr) {
     let arrayList = [...arguments];
     let newArr = [];
@@ -26,11 +35,4 @@ function uniteUnique(arr) {
   
   uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
   
-  // problem domain
-  /*
-  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
-  uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
-  uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
-  uniteUnique([1, 3, 2], [5, 4], [5, 6]) should return [1, 3, 2, 5, 4, 6].
-  uniteUnique([1, 3, 2, 3], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
-  */
+  
