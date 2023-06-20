e1be809c4003ced97c0fdb00cb8b1e3f6cfbe22d
Ladzi <ladziladzani@gmail.com>
2023-06-20 20:46:52 +0200

    revision on some of javaScript array-methods

---
+++ fundamentals/arrayMethods.js
@@ -0,0 +1,21 @@
+/*
+Array length
+Array toString()
+Array pop()
+Array push()
+Array shift()
+Array unshift()
+Array join()
+Array delete()
+Array concat()
+Array flat()
+Array splice()
+Array slice()
+
+brushing over some of array-methods
+*/
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for(let i =0; i < arr.length; i++){
+    console.log(arr[i]);
+}
